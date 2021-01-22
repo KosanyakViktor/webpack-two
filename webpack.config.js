2dadfb4bc8c05e4bb3ae9de76439708389037642
Viktor Kosanyak <kosanyakviktor@gmail.com>
2021-01-22 15:10:17 +0200

    Task3

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,54 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                use: [
-                    {
-                        loader:'url-loader',
-                        options: {
-                            outputPath: 'images',
-                            limit: 8192,
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+    const config = {
+        entry: './src/index.js',
+        output: {
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s?css$/i,
+                    use: [
+                        isProduction 
+                          ? MiniCssExtractPlugin.loader 
+                          :'style-loader',
+                           'css-loader', 
+                           'sass-loader'
+                        ]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/i,
+                    use: [
+                        {
+                            loader:'url-loader',
+                            options: {
+                                outputPath: 'images',
+                                limit: 8192,
+                            }
                         }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        })
-    ]
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+        ],
+        devServer: {
+            port: 9000,
+            hot: true,
+        }
+    }
+
+    if(isProduction) {
+         config.plugins.push(new MiniCssExtractPlugin({
+             filename: '[name].css'
+         }))
+    }
+    return config;
 };
